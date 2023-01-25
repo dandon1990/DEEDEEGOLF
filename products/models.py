d98385effac1d384c7a2a28ab49c28b1cad7d6d7
dandon1990 <daniel.don@icloud.com>
2023-01-25 19:16:18 +0000

    Added products app and models

---
+++ products/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+
+
+BALL_COVER = ((0, "Soft"), (1, "Hard"))
+
+class Category(models.Model):
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+class Product(models.Model):
+    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    name = models.CharField(max_length=254)
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    brand = models.CharField(max_length=254)
+    has_loft = models.BooleanField(default=False, null=True, blank=True)
+    ball_softness = models.IntegerField(choices=BALL_COVER, default=0, null=True, blank=True)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
