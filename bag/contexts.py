f7f37945d84606e47d06c4549b814cd58986fbab
dandon1990 <daniel.don@icloud.com>
2023-01-28 20:06:16 +0000

    updated product size logic

--- bag/contexts.py
+++ bag/contexts.py
@@ -1,3 +1,99 @@
+# from decimal import Decimal
+# from django.conf import settings
+# from django.shortcuts import get_object_or_404
+# from products.models import Product
+
+# def bag_contents(request):
+
+#     bag_items = []
+#     total = 0
+#     product_count = 0
+#     bag = request.session.get('bag', {})
+#     # d_size = 0
+#     # if 'product_d_loft' in request.POST:
+#     #     d_size = request.POST['product_d_loft']
+#     # w_size = 0
+#     # if 'product_w_loft' in request.POST:
+#     #     w_size = request.POST['product_w_loft']
+#     # # d_size = request.POST.get('product_d_loft')
+#     # # w_size = request.POST.get('product_w_loft')
+
+#     # for item_id, item_data in bag.items():
+
+#     #     if isinstance(item_data, int):
+#     #         product = get_object_or_404(Product, pk=item_id)
+#     #         total += item_data * product.price
+#     #         product_count += item_data
+#     #         bag_items.append({
+#     #             'item_id': item_id,
+#     #             'quantity': item_data,
+#     #             'product': product,
+#     #         })
+#     #     else:
+#     #         if d_size:
+#     #             product = get_object_or_404(Product, pk=item_id)
+#     #             for d_size, quantity in item_data['items_by_d_size'].items():
+#     #                 total += quantity * product.price
+#     #                 product_count += quantity
+#     #                 bag_items.append({
+#     #                     'item_id': item_id,
+#     #                     'quantity': item_data,
+#     #                     'product': product,
+#     #                     'd_size': d_size,
+#     #                 })
+#     #         elif w_size:        
+#     #             for w_size, quantity in item_data['items_by_w_size'].items():
+#     #                 total += quantity * product.price
+#     #                 product_count += quantity
+#     #                 bag_items.append({
+#     #                     'item_id': item_id,
+#     #                     'quantity': item_data,
+#     #                     'product': product,
+#     #                     'w_size': w_size, 
+#     #                 })
+#     for item_id, item_data in bag.items():
+#         if isinstance(item_data, int):
+#             product = get_object_or_404(Product, pk=item_id)
+#             total += item_data * product.price
+#             product_count += item_data
+#             bag_items.append({
+#                 'item_id': item_id,
+#                 'quantity': item_data,
+#                 'product': product,
+#             })
+#         else:
+#             product = get_object_or_404(Product, pk=item_id)
+#             for size, quantity in item_data['items_by_size'].items():
+#                 total += quantity * product.price
+#                 product_count += quantity
+#                 bag_items.append({
+#                     'item_id': item_id,
+#                     'quantity': item_data,
+#                     'product': product,
+#                     'size': size,
+#                 })
+
+#     if total < settings.FREE_DELIVERY_THRESHOLD:
+#         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
+#         free_delivery_delta = settings.FREE_DELIVERY_THRESHOLD - total
+#     else:
+#         delivery = 0
+#         free_delivery_delta = 0
+    
+#     grand_total = delivery + total
+
+#     context = {
+#         'bag_items': bag_items,
+#         'total': total,
+#         'product_count': product_count,
+#         'delivery': delivery,
+#         'free_delivery_delta': free_delivery_delta,
+#         'free_delivery_threshold': settings.FREE_DELIVERY_THRESHOLD,
+#         'grand_total': grand_total,
+#     }
+
+#     return context
+
 from decimal import Decimal
 from django.conf import settings
 from django.shortcuts import get_object_or_404
@@ -10,15 +106,27 @@ def bag_contents(request):
     product_count = 0
     bag = request.session.get('bag', {})
 
-    for item_id, quantity in bag.items():
-        product = get_object_or_404(Product, pk=item_id)
-        total += quantity * product.price
-        product_count += quantity
-        bag_items.append({
-            'item_id': item_id,
-            'quantity': quantity,
-            'product': product,
-        })
+    for item_id, item_data in bag.items():
+        if isinstance(item_data, int):
+            product = get_object_or_404(Product, pk=item_id)
+            total += item_data * product.price
+            product_count += item_data
+            bag_items.append({
+                'item_id': item_id,
+                'quantity': item_data,
+                'product': product,
+            })
+        else:
+            product = get_object_or_404(Product, pk=item_id)
+            for size, quantity in item_data['club_by_size'].items():
+                total += quantity * product.price
+                product_count += quantity
+                bag_items.append({
+                    'item_id': item_id,
+                    'quantity': item_data,
+                    'product': product,
+                    'size': size,
+                })
 
     if total < settings.FREE_DELIVERY_THRESHOLD:
         delivery = total * Decimal(settings.STANDARD_DELIVERY_PERCENTAGE / 100)
@@ -28,7 +136,7 @@ def bag_contents(request):
         free_delivery_delta = 0
     
     grand_total = delivery + total
-
+    
     context = {
         'bag_items': bag_items,
         'total': total,
--- bag/views.py
+++ bag/views.py
@@ -1,23 +1,94 @@
+# from django.shortcuts import render, redirect
+
+# # Create your views here.
+
+# def view_bag(request):
+#     """A view that renders the shopping bag contents"""
+
+#     return render(request, 'bag/bag.html')
+
+# def add_to_bag(request, item_id):
+#     """Add a quantity of the specified product to the shopping bag """
+
+#     # quantity = int(request.POST.get('quantity'))
+#     # redirect_url = request.POST.get('redirect_url')
+#     # d_size = 0
+#     # if 'product_d_loft' in request.POST:
+#     #     d_size = request.POST['product_d_loft']
+#     # w_size = 0
+#     # if 'product_w_loft' in request.POST:
+#     #     w_size = request.POST['product_w_loft']
+#     # bag = request.session.get('bag', {})
+
+#     # if d_size:
+#     #     if item_id in list(bag.keys()):
+#     #         if d_size in bag[item_id]['items_by_d_size'].keys():
+#     #             bag[item_id]['items_by_d_size'][d_size] += quantity
+#     #         else:
+#     #             bag[item_id]['items_by_d_size'][d_size] = quantity
+#     #     else:
+#     #         bag[item_id] = {'items_by_d_size': {d_size: quantity}}
+
+#     # elif w_size:
+#     #     if item_id in list(bag.keys()):
+#     #         if w_size in bag[item_id]['items_by_w_size'].keys():
+#     #             bag[item_id]['items_by_w_size'][w_size] += quantity
+#     #         else:
+#     #             bag[item_id]['items_by_w_size'][w_size] = quantity
+#     #     else:
+#     #         bag[item_id] = {'items_by_w_size': {w_size: quantity}}  
+
+#     # else:
+#     #     if item_id in list(bag.keys()):
+#     #             bag[item_id] += quantity
+#     #     else:
+#     #         bag[item_id] = quantity
+    
+#     # print(bag)
+
+#     request.session['bag'] = bag
+#     # print(request.POST)
+#     return redirect(redirect_url)
 from django.shortcuts import render, redirect
 
 # Create your views here.
 
 def view_bag(request):
-    """A view that renders the shopping bag contents"""
+    """ A view that renders the bag contents page """
 
     return render(request, 'bag/bag.html')
 
 def add_to_bag(request, item_id):
-    """Add a quantity of the specified product to the shopping bag """
+    """ Add a quantity of the specified product to the shopping bag """
 
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
+    d_size = None
+    if 'product_loft' in request.POST:
+        d_size = request.POST['product_loft']
     bag = request.session.get('bag', {})
 
-    if item_id in list(bag.keys()):
-        bag[item_id] += quantity
+    if d_size:
+        if item_id in list(bag.keys()):
+            if d_size in bag[item_id]['club_by_size'].keys():
+                bag[item_id]['club_by_size'][d_size] += quantity
+            else:
+                bag[item_id]['club_by_size'][d_size] = quantity
+        else:
+            bag[item_id] = {'club_by_size': {d_size: quantity}}
+    # elif w_size:
+    #     if item_id in list(bag.keys()):
+    #         if w_size in bag[item_id]['items_by_size'].keys():
+    #             bag[item_id]['items_by_size_w'][w_size] += quantity
+    #         else:
+    #             bag[item_id]['items_by_size_w'][w_size] = quantity
+    #     else:
+    #         bag[item_id] = {'items_by_size_w': {w_size: quantity}}
     else:
-        bag[item_id] = quantity
+        if item_id in list(bag.keys()):
+            bag[item_id] += quantity
+        else:
+            bag[item_id] = quantity
 
     request.session['bag'] = bag
     return redirect(redirect_url)
