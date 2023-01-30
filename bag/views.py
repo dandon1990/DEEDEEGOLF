4b46cfe475b7b0cc8752763a48f4099326d02717
dandon1990 <daniel.don@icloud.com>
2023-01-30 19:00:06 +0000

    Added adjust and remove from bag views, and calcsubtotal template filter

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse, HttpResponse
 
 # Create your views here.
 
@@ -33,4 +33,52 @@ def add_to_bag(request, item_id):
 
     request.session['bag'] = bag
     return redirect(redirect_url)
+
+
+def adjust_bag(request, item_id):
+    """ Adjust the quantity of the specified product to the speicified amount """
+
+    quantity = int(request.POST.get('quantity'))
+    loft = None
+    if 'product_loft' in request.POST:
+        loft = request.POST['product_loft']
+    bag = request.session.get('bag', {})
+
+    if loft:
+        if quantity > 0:
+            bag[item_id]['clubs_by_loft'][loft] = quantity
+        else:
+            del bag[item_id]['clubs_by_loft'][loft]
+            if not bag[item_id]['clubs_by_loft']:
+                bag.pop(item_id)
+    else:
+        if quantity > 0:
+            bag[item_id] = quantity
+        else:
+            bag.pop(item_id)
+
+    request.session['bag'] = bag
+    return redirect(reverse('view_bag'))
+
+def remove_from_bag(request, item_id):
+    """ Remove the item from the shopping bagS """
+    try:
+        loft = None
+        if 'product_loft' in request.POST:
+            loft = request.POST['product_loft']
+        bag = request.session.get('bag', {})
+
+        if loft:
+            del bag[item_id]['clubs_by_loft'][loft]
+            if not bag[item_id]['clubs_by_loft']:
+                bag.pop(item_id)
+
+        else:
+            bag.pop(item_id)
+
+        request.session['bag'] = bag
+        return HttpResponse(status=200)
+
+    except Exception as e:
+        return HttpResponse(status=500)
     
